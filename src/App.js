0dfd90c1ef66a47807d3e7306191e0ce0183ce85
amitsingh0123 <amitsingh63051@gmail.com>
2025-01-21 19:17:27 +0530

    Header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './GlobalComponents/Header/Header'
+import Footer from './GlobalComponents/Footer/Footer'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div className="App">
-     
+      <Header/>
+      |<Hero/>
+      <Footer/>
     </div>
   );
 }
